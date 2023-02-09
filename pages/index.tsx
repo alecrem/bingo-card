cf1046c5f1a08f0bb9191e004871484a63f5c7ea
Alejandro Cremades <ale@alecrem.com>
2023-02-10 00:09:02 +0900

    Added p5.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,36 @@
 import Head from 'next/head'
 import { Box, Container, Heading } from '@chakra-ui/react'
+import p5Types from 'p5'
+import dynamic from 'next/dynamic'
+const importFunction = () => import('react-p5').then((mod) => mod.default)
+const Sketch = dynamic(importFunction, { ssr: false })
+
+let x = 50
+const y = 50
 
 export default function Home() {
+  const setup = (p5: p5Types, canvasParentRef: Element) => {
+    let parentStyle: CSSStyleDeclaration
+    if (canvasParentRef.parentElement) {
+      parentStyle = getComputedStyle(canvasParentRef.parentElement)
+    } else {
+      parentStyle = getComputedStyle(canvasParentRef)
+    }
+    const canvasWidth = parseInt(parentStyle.width)
+    const canvasHeight = parseInt(parentStyle.height)
+    console.log('canvas size', canvasWidth, canvasHeight)
+    const myCanvas = p5
+      .createCanvas(canvasWidth, canvasHeight)
+      .parent(canvasParentRef)
+    myCanvas.id('asjalk')
+  }
+
+  const draw = (p5: p5Types) => {
+    p5.background(0)
+    p5.ellipse(x, y, 70, 70)
+    x++
+  }
+
   return (
     <>
       <Box>
@@ -9,6 +38,9 @@ export default function Home() {
           <Heading as="h1" size="4xl">
             Gamerah Bingo
           </Heading>
+          <Box bg="#eee" w="100%" h="300px" p={0} mt={4} mb={4}>
+            <Sketch setup={setup} draw={draw} />
+          </Box>
         </Container>
       </Box>
     </>
