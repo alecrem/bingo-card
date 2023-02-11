de1ccaee5fe8d8a44ee6ba1deaaf1402f5370ac2
Alejandro Cremades <ale@alecrem.com>
2023-02-11 23:21:17 +0900

    Responsive layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
-import { Box, Container, Heading } from '@chakra-ui/react'
+import { Box, AspectRatio, Container, Heading } from '@chakra-ui/react'
 import dynamic from 'next/dynamic'
+import { useMounted } from '@/hooks/useMounted'
 import { setup, draw } from '@/p5js/sketch'
 
 const importFunction = () => import('react-p5').then((mod) => mod.default)
@@ -9,17 +10,22 @@ if (typeof window !== 'undefined') {
 }
 
 export default function Home() {
+  const isMounted = useMounted()
   return (
     <>
       <Box>
-        <Container maxW="container.sm" mt="4em">
-          <Heading as="h1" size="4xl">
+        <Container maxW="container.sm" mt="2em" mb="2em">
+          <Heading as="h1" size="4xl" mb="0.5em">
             Gamerah Bingo
           </Heading>
-          <Box bg="#eee" w="100%" h="300px" p={0} mt={4} mb={4}>
-            {Sketch && <Sketch setup={setup} draw={draw} />}
-          </Box>
         </Container>
+        {isMounted && (
+          <Container maxW="container.sm" mt="2em" mb="2em" pl={0} pr={0}>
+            <AspectRatio maxW="container.sm" ratio={1}>
+              <Sketch setup={setup} draw={draw} />
+            </AspectRatio>
+          </Container>
+        )}
       </Box>
     </>
   )
